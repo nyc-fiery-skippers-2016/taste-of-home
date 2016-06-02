d2b087fb7f5328535c2dc1ea7d86309c9e1ca751
Megan Swanby <Megan.Swanby@gmail.com>
2016-06-02 18:28:45 -0400

    Add seed file with users and initial tags, no stores as they will come from the APIs. Tested for associations.

--- taste_of_home/db/seeds.rb
+++ taste_of_home/db/seeds.rb
@@ -3,5 +3,27 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+User.delete_all
+Tag.delete_all
+
+
+
+users = 20.times.map do
+  User.create!( :username => Faker::Internet.user_name,
+                :email => Faker::Internet.email,
+                :password => "password",
+    )
+end
+
+Tag.create!(description: 'British')
+Tag.create!(description: 'Senegal')
+Tag.create!(description: 'candy')
+Tag.create!(description: 'spices')
+Tag.create!(description: 'Vietnam')
+Tag.create!(description: 'French')
+Tag.create!(description: 'bread')
+Tag.create!(description: 'Brazil')
+Tag.create!(description: 'New Zealand')
+
