8643bd25b925875eabfba6f6d38de8d3e6d41644
Evan Druce <drucee@gmail.com>
2016-06-02 17:51:50 -0400

    update join table migrations and fix associations

--- taste_of_home/db/migrate/20160602212440_create_store_users.rb
+++ taste_of_home/db/migrate/20160602212440_create_store_users.rb
@@ -1,6 +1,8 @@
 class CreateStoreUsers < ActiveRecord::Migration
   def change
     create_table :store_users do |t|
+      t.references :user, foreign_key: true
+      t.references :store, foreign_key: true
 
       t.timestamps null: false
     end
--- taste_of_home/db/migrate/20160602212503_create_store_tags.rb
+++ taste_of_home/db/migrate/20160602212503_create_store_tags.rb
@@ -1,6 +1,8 @@
 class CreateStoreTags < ActiveRecord::Migration
   def change
     create_table :store_tags do |t|
+      t.references :store, foreign_key: true
+      t.references :tag, foreign_key: true
 
       t.timestamps null: false
     end
