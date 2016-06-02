0f5edb0c206bf814b456227b8d3543a7139bd48d
Megan Swanby <Megan.Swanby@gmail.com>
2016-06-02 19:22:06 -0400

    Add new files with postgres
    We need to switch all of our work to this set of files. Seed files, migrations and models have been added. When you pull your current branch I will migrate the files and test them. Tomorrow we'll delete and only work from this version.

---
+++ taste_of_home/db/migrate/20160602230648_create_store_tags.rb
@@ -0,0 +1,10 @@
+class CreateStoreTags < ActiveRecord::Migration
+  def change
+    create_table :store_tags do |t|
+      t.references :store, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
