7f2d6b94c7a3d9b25aca9e90f2970ef204116273
Megan Swanby <Megan.Swanby@gmail.com>
2016-06-02 20:24:40 -0400

    Update routes to include all user routes

--- taste_of_home/config/routes.rb
+++ taste_of_home/config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
 
 
-
-
-
-
-
-
   get '/' => 'stores#index'
-
-  get '/users/:id' => 'users#show'
 
   get '/stores/:id' => 'stores#show'
 
@@ -17,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'stores#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -26,7 +18,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :users
 
   # Example resource route with options:
   #   resources :products do
