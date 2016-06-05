a136e18ec196c86620ceb3b7b5c8b6be3ccc2d31
Adam Zmudzinski <email": null>
2016-06-05 18:50:20 -0400

    Fix uniqueness

--- db/migrate/20160605191612_create_lists.rb
+++ db/migrate/20160605191612_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
       t.text :description, null: false
 
       t.references :user, null: false
