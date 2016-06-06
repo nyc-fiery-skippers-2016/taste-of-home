0ebde4cc3b394e727ebe74a7552eebf8f62d44ca
Saralis Rivera <saralis.rivera@gmail.com>
2016-06-06 18:12:26 -0400

    remove old commented out code in store controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -27,17 +27,6 @@ class StoresController < ApplicationController
     render json: stores
 
     end
-  #   new_results = {}
-
-  #   new_results[:businesses] = []
-  #   new_results[:latitude] = results.region.center.latitude
-  #   new_results[:longitude] = results.region.center.longitude
-  #   new_results[:latitude_delta] = results.region.span.latitude_delta
-  #   new_results[:longitude_delta] = results.region.span.longitude_delta
-  #   results.businesses.each do |business|
-  #     new_results[:businesses] << {address: business.location.address[0], city: business.location.city, country_code: business.location.country_code, name: business.name, id: business.id, image_url: business.image_url, rating_img_url: business.rating_img_url, review_count: business.review_count}
-  #   end
-
 
 
   private
