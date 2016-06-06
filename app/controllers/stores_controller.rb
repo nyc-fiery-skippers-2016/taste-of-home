d3ee9329347d0cdbd9639b1d38efaa29fa42083c
Megan Swanby <Megan.Swanby@gmail.com>
2016-06-06 11:33:02 -0400

    Update migrations to include lat and long delta and review count. Add dot env gem and fix store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -11,30 +11,42 @@ class StoresController < ApplicationController
 
 
   def search
+    # using the search term to find the business from yelp
     parameters = { term: params[:term], category_filter: params[:category_filter], limit: 5, location: params[:location] }
     results = Yelp.client.search(params[:location], parameters)
-    new_results = {}
 
-    new_results[:businesses] = []
-    new_results[:latitude] = results.region.center.latitude
-    new_results[:longitude] = results.region.center.longitude
-    new_results[:latitude_delta] = results.region.span.latitude_delta
-    new_results[:longitude_delta] = results.region.span.longitude_delta
-    results.businesses.each do |business|
-      new_results[:businesses] << {address: business.location.address[0], city: business.location.city, country_code: business.location.country_code, name: business.name, id: business.id, image_url: business.image_url, rating_img_url: business.rating_img_url, review_count: business.review_count}
+    stores = results.businesses.map do |business|
+      {name: business.name, address: business.location.display_address.push(business.location.country_code), phone: business.display_phone, description: business.categories.flatten, longitude: business.location.coordinate.longitude, latitude: business.location.coordinate.latitude, img_url: business.image_url, rating_url: business.rating_img_url, yelp_id: business.id, longitude_delta: results.region.span.longitude_delta, latitude_delta: results.region.span.latitude_delta, review_count: business.review_count}
     end
 
-    @stores = []
-    new_results[:businesses].each do |business|
-      store = Store.find_by(address: business[:id])
-        unless store
-          store = Store.new(name: business[:name], address: business[:id])
-          store.save
-        end
+    stores.each do |store|
 
-      @stores << store
-      end
-    render json: new_results
+     unless Store.exists?(yelp_id: store[:yelp_id])
+      Store.create(store)
+     end
+
+    end
+  #   new_results = {}
+
+  #   new_results[:businesses] = []
+  #   new_results[:latitude] = results.region.center.latitude
+  #   new_results[:longitude] = results.region.center.longitude
+  #   new_results[:latitude_delta] = results.region.span.latitude_delta
+  #   new_results[:longitude_delta] = results.region.span.longitude_delta
+  #   results.businesses.each do |business|
+  #     new_results[:businesses] << {address: business.location.address[0], city: business.location.city, country_code: business.location.country_code, name: business.name, id: business.id, image_url: business.image_url, rating_img_url: business.rating_img_url, review_count: business.review_count}
+  #   end
+
+    render json: stores
   end
+
+  private
+    # def set_store
+    #   @store = Store.find(params[:id])
+    # end
+
+    def store_params
+      params.require(:store).permit(:name, :address, :description, :email, :phone, :longitude, :latitude, :image_url, :rating_url, :yelp_id, :favorite, :longitude_delta, :latitude_delta, :review_count)
+    end
 
 end
--- db/migrate/20160602230554_create_stores.rb
+++ db/migrate/20160602230554_create_stores.rb
@@ -1,15 +1,18 @@
 class CreateStores < ActiveRecord::Migration
   def change
     create_table :stores do |t|
-      t.string :name, null: false
-      t.text :description
-      t.string :address, null: false
+      t.string  :name, null: false
+      t.text    :description
+      t.string  :address, null: false
       t.integer :phone
-      t.string :email
-      t.string :coordinate, null: false
-      t.string :photo_url
-      t.string :rating_url
-      t.string :yelp_id, null: false
+      t.string  :email
+      t.float   :longitude, null: false
+      t.float   :latitude, null: false
+      t.float   :longitude_delta, null: false
+      t.float   :latitude_delta, null: false
+      t.string  :img_url
+      t.string  :rating_url
+      t.string  :yelp_id, null: false
       t.boolean :favorite, default: false
 
       t.timestamps null: false
