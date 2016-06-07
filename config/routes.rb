0b913c6ff28fe8c81594cb7f3d32be4090126df5
Saralis Rivera <saralis.rivera@gmail.com>
2016-06-06 21:05:35 -0400

    fix repeating results on index

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post '/stores/:yelp_id/comments' => 'comments#create'
 
   get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
+  post 'login' => 'sessions#cresate'
   get 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
