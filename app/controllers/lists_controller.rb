28411756ab7804a79f3768b233e6c0ad289f8aa7
NYCSwan <megan.swanby@gmail.com>
2016-06-07 15:38:35 -0400

    update user show page and view for tags and lists.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,8 @@
 class ListsController < ApplicationController
 
+  def new
+    @list = List.new
+  end
 
   def create
     List.create(list_params)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,14 @@ class UsersController < ApplicationController
   def show
     # if logged_in? && current_user == @user.id
       @user = User.find(params[:id])
-   
+ 
     # @stores = []
     # if logged_in?
     #   StoreUser.where(user_id: current_user.id).each do |store_user|
     #     @stores.push(Store.find_by(id: store_user.store_id))
     #   end
-    #   @list = List.new
+      @list = List.new
+      @lists = List.where(user_id: @user.id)
     # end
   end
 
