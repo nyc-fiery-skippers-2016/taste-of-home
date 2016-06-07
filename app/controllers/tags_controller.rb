c6a09e84a24a8be5ae580661357210987e998610
NYCSwan <megan.swanby@gmail.com>
2016-06-07 11:48:56 -0400

    Edit tags to list all on index and add new route

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,14 +2,11 @@ class TagsController < ApplicationController
 
   def index
     @tags = Tag.all
-    if params[:tag]
-      tag = Tag.find_by(name: params[:tag])
-      @stores = tag.stores if tag
-    end
   end
 
   def show
     @tag = Tag.find_by(id: params[:id])
+    @stores = @tag.stores
   end
 
 end
