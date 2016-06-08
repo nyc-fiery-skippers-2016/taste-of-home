7911352fb4553a463bdf6e48e87e63c8faaf529c
Saralis Rivera <saralis.rivera@gmail.com>
2016-06-08 01:21:59 -0400

    working ajax to display form only

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,1 +1,24 @@
-comments.js
+$(document).ready(function(){
+  $('#comment-link').on('click', function(event){
+    event.preventDefault();
+
+    $target = $(event.target)
+    var yelpId = $target.attr('href').split('/')[2]
+    var url = yelpId + '/comments/new'
+    var data = $target.serialize();
+
+    $.ajax({
+      url: url,
+      method: 'GET',
+      data: data,
+    }).done(function(response){
+      $('#form').show();
+      $('#comment-link').hide();
+      $('#comments').prepend(response)
+
+
+    })
+  })
+  // $('#comments').prepend(response)
+
+})
