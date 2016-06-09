a1209346ef8e6a567363e9c9efeeab82abdec4f6
Evan Druce <drucee@gmail.com>
2016-06-09 12:11:49 -0400

    create css structure for displaying user lists

---
+++ db/migrate/20160609153106_add_image_path_to_lists.rb
@@ -0,0 +1,4 @@
+class AddImagePathToLists < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606152852) do
+ActiveRecord::Schema.define(version: 20160609153106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
